e27391e5ab48431b4c5053fe9fbaf8c9f4c4c0a8
Anika Friedman <anika.friedman@colorado.edu>
2022-09-07 18:04:39 -0400

    Fix errors

--- example/prot_water_inter.py
+++ example/prot_water_inter.py
@@ -17,7 +17,7 @@ import os.path
 parser = argparse.ArgumentParser(description = 'Determination of DSSP, H-bonds, Ligand Contacts, protein and ligand RMSD, Helical interactions and PCA for GROMACS Trajectory of PTP1B')
 parser.add_argument('-t', required=True, help='File name for input trajectory')
 parser.add_argument('-g', required=True, help= 'File name for input topology (gro format)')
-parser.add_argument('-m', required=False, default = 0, help= 'Supply the number of missing terminal residues')
+parser.add_argument('-m', required=False, default = 0, help= 'Supply the number of missing terminal residues(default 0)')
 
 #Import Arguments
 args = parser.parse_args()
@@ -30,10 +30,11 @@ if File_gro.split('.')[-1] != 'gro': #Add default file extension if not in input
 miss_res = args.m
 
 #Source custom functions
-sys.path.insert(1, '$PROJECT/code/MDTrajAnalysis/Traj_process/')
+prefix = '/ocean/projects/cts160011p/afriedma/code/MDTrajAnalysis/'
+sys.path.insert(1, prefix + 'Traj_process/')
 import traj 
 
-sys.path.insert(1, '$PROJECT/code/MDTrajAnalysis/protein_inter/')
+sys.path.insert(1, prefix + 'protein_inter/')
 import water_inter
 
 #Load Trajectory
@@ -42,7 +43,13 @@ traj = traj.mdtraj_load(File_traj, File_gro)
 #Set protein offset based on missing residues
 offset = 1 + miss_res
 
+#Compute neighboring water molecules to residue of interest
+water_indices = water_inter.water_neighbor(traj, 121, offset)
+
 #Determine the distance b/w all water and protein residues
-water_dist = water_inter.prot_water_dist(traj, [0,180], offset)
+water_dist = water_inter.prot_water_dist(traj, 121, offset, water_indices)
+
+#Determine Number of H-bonds and VDW interactions b/w residue and water molecules in each frame
+
 
 print(np.shape(water_dist))
--- protein_inter/water_inter.py
+++ protein_inter/water_inter.py
@@ -1,18 +1,42 @@
+def traj_sect(traj, prot_res, offset):
+    import mdtraj as md
+    res = prot_res - offset
+    top = traj.topology
+    traj_sect = top.select('resid ' + str(res)) #Select only atoms in the given section
+    return traj_sect
 
-def prot_water_dist(traj, prot_res, offset):
-    from itertools import product
+def water_neighbor(traj, prot_res, offset):
+    import mdtraj as md
+    import numpy as np
 
     #Seperate protein residues of interest
-    res1 = prot_res[0] - offset
-    res2 = prot_res[1] - offset
-    top = traj.topology()
-    traj_sect = top.select(str(res1) + ' <= resid and resid <= ' + str(res2)) #Select only atoms in the given section
+    prot_ind = traj_sect(traj, prot_res, offset)
+
+    #Compute neighboring atoms for all residues of interest
+    neighbors = md.compute_neighbors(traj, 0.5, prot_ind, haystack_indices=None, periodic=True)
     
-    #Seperate solvent molecules
-    traj_solv = top.select('resname SOL')
-    
+    #Get list of all unique neighboring water atoms
+    top = traj.topology
+    traj_solv = top.select('water')
+    water_indices = []
+    for i in range(len(neighbors)):
+        indices = neighbors[i]
+        for j in range(len(indices)):
+            if indices[j] not in water_indices and indices[j] in traj_solv:
+                water_indices.append(indices[j])
+    print(len(water_indices))
+    return water_indices
+
+def prot_water_dist(traj, prot_res, offset, water_indices):
+    from itertools import product
+    import mdtraj as md
+    import numpy as np
+
+    #Seperate protein residues of interest
+    prot_ind = traj_sect(traj, prot_res, offset)
+
     #Determine all solvent protein residue pairs
-    prot_solv = list(product(traj_select, traj_solv))
+    prot_solv = list(product(prot_ind, water_indices))
 
     #Determine distance b/w all protein residues and all solvent molecules
     [dist, pairs] = md.compute_contacts(traj, contacts=prot_solv, scheme='closest-heavy', ignore_nonprotein = False, periodic=True, soft_min = False)
