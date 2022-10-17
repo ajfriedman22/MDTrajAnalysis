39c27126a55c232b7b79dee985f836142dc41b70
Anika Friedman <anika.friedman@colorado.edu>
2022-10-17 11:21:44 -0400

    Add checks and verify output

--- example/prot_water_inter.py
+++ example/prot_water_inter.py
@@ -18,7 +18,7 @@ parser = argparse.ArgumentParser(description = 'Determination of DSSP, H-bonds, 
 parser.add_argument('-t', required=True, help='File name for input trajectory')
 parser.add_argument('-g', required=True, help= 'File name for input topology (gro format)')
 parser.add_argument('-m', required=False, type=int, default = 0, help= 'Supply the number of missing terminal residues(default 0)')
-parser.add_argument('-df', required=True, help= 'Directory github repo is in')
+parser.add_argument('-l', required=False, type=int, default = 0, help= 'Ligand residue ID')
 
 #Import Arguments
 args = parser.parse_args()
@@ -29,14 +29,15 @@ File_gro = args.g
 if File_gro.split('.')[-1] != 'gro': #Add default file extension if not in input
     File_gro = File_gro + '.gro'
 miss_res = args.m
-directory = args.df
+lig = args.l
 
 #Source custom functions
-prefix = directory + '/MDTrajAnalysis/'
-sys.path.insert(1, prefix + 'Traj_process/')
+current_directory = os.path.dirname(os.path.realpath(__file__))
+prefix = current_directory.rsplit('/',1)[0]
+sys.path.insert(1, prefix + '/Traj_process/')
 import load_data 
 
-sys.path.insert(1, prefix + 'protein_inter/')
+sys.path.insert(1, prefix + '/protein_inter/')
 import water_inter
 
 #Load Trajectory
@@ -45,19 +46,65 @@ traj = load_data.mdtraj_load(File_traj, File_gro)
 #Set protein offset based on missing residues
 offset = 1 + miss_res
 
+#Test that ligand ID Assigned properly
+test = traj.topology.select('resid ' + str(lig) + ' and resname LIG')
+if test.size==0:
+    print('Ligand not named correctly! Exiting Immediately!')
+    exit()
+
+#Determine water ligand contacts
+if lig != 0:
+    lig = lig
+    water_indices, water_per_lig, water_neighbors = water_inter.water_neighbor(traj, lig, offset, False, 530)
+
+    #Put residue indices in numerical order
+    water_indices.sort()
+
+    #Determine % contact with water residues
+    water_res_per = np.zeros(len(water_indices))
+    for j in range(len(water_neighbors)):#Loop through frames
+        contact = water_neighbors[j]
+        for n in range(len(contact)): #Loop through water interactions in the frame
+            for i in range(len(water_indices)): #Loop through water residues to determine which is matched in frame
+                if contact[n] == water_indices[i]:
+                    water_res_per[i] += 1
+
+    water_res_per = 100*(water_res_per/len(water_neighbors))
+
+    #Output contacts
+    output = open('Ligand_water_per.txt', 'w')
+    output_high = open('Ligand_water_high.txt', 'w')
+    for j in range(len(water_indices)):
+        if water_res_per[j] > 10:
+            output_high.write(str(water_indices[j]) + ': ' + str(water_res_per[j]) + '\n')
+        output.write(str(water_indices[j]) + ': ' + str(water_res_per[j]) + '\n')
+    output.close()
+    output_high.close()
+    print('Ligand Analysis Complete')
+
 #Set residues of interest
-res_interest = [121]
+res_interest = np.arange(1, lig-1, 1)
+water_per_res = np.zeros(len(res_interest))
+lig_res_per = np.zeros(len(res_interest))
 
 #Loop through each residue of interest
-for i in res_interest:
+for i in range(len(res_interest)):
     #Compute neighboring water molecules to residue of interest
-    water_indices = water_inter.water_neighbor(traj, 121, offset)
-    
-    #Determine all h-bonds present
-    #Determine the distance b/w all water and protein residues
-    water_dist = water_inter.prot_water_dist(traj, 121, offset, water_indices)
-    
-    #Determine Number of H-bonds and VDW interactions b/w residue and water molecules in each frame
-    
+    water_indices, water_per_res[i], lig_res_water_cont = water_inter.water_neighbor(traj, res_interest[i], offset, water_neighbors, 530)
+    lig_res_per[i] = 100*sum(lig_res_water_cont)/len(lig_res_water_cont)
 
-    print(np.shape(water_dist))
+#Determine residues with highest number of water molecules interacting
+max_water = max(water_per_res)
+res_max_water = []
+for i in range(len(res_interest)):
+    if water_per_res[i] > 20:
+        res_max_water.append(res_interest[i])
+np.savetxt('res_max_water.txt', res_max_water)
+
+#Ouput residues which simultaneously interact with the same water molecules as the ligand
+output = open('lig_res_water.txt', 'w')
+for i in range(len(lig_res_per)):
+    if lig_res_per[i] > 25:
+        output.write(str(res_interest[i]) + ': ' + str(lig_res_per[i]) + '\n')
+np.savetxt('lig_res_water_per.txt', lig_res_per)
+print('Protein Analysis Complete')
--- protein_inter/water_inter.py
+++ protein_inter/water_inter.py
@@ -5,7 +5,7 @@ def traj_sect(traj, prot_res, offset):
     traj_sect = top.select('resid ' + str(res)) #Select only atoms in the given section
     return traj_sect
 
-def water_neighbor(traj, prot_res, offset):
+def water_neighbor(traj, prot_res, offset, lig_res_water_neighbor, res_not_water):
     import mdtraj as md
     import numpy as np
 
@@ -13,18 +13,77 @@ def water_neighbor(traj, prot_res, offset):
     prot_ind = traj_sect(traj, prot_res, offset)
 
     #Compute neighboring atoms for all residues of interest
-    neighbors = md.compute_neighbors(traj, 0.5, prot_ind, haystack_indices=None, periodic=True)
+    neighbors = md.compute_neighbors(traj, 0.4, prot_ind, haystack_indices=None, periodic=True)
     
-    #Get list of all unique neighboring water atoms
+    #Determine which neighbors are water molecules
     top = traj.topology
     traj_solv = top.select('water')
-    water_indices = []
+
+    water_neighbors = []
     for i in range(len(neighbors)):
-        indices = neighbors[i]
-        for j in range(len(indices)):
-            if indices[j] not in water_indices and indices[j] in traj_solv:
-                water_indices.append(indices[j])
-    return water_indices
+        water_neighbors_i = []
+        neighbors_i = neighbors[i]
+        for j in neighbors_i:
+            if j in traj_solv:
+                water_neighbors_i.append(j)
+        #Add array of water neighbors to full list
+        water_neighbors.append(water_neighbors_i)
+    #Delete neighbors array as no longer needed
+    del neighbors
+
+    #Determine residue from atom number
+    water_res_neighbors = []
+    traj_OW = top.select('water and name O')
+    traj_HW1 = top.select('water and name H1')
+    traj_HW2 = top.select('water and name H2')
+    traj_not_water = top.select('protein or resname LIG')
+    for i in range(len(water_neighbors)):
+        water_neighbors_i = water_neighbors[i]
+        water_res_neighbors_i = []
+        for j in water_neighbors_i:
+            if j in traj_OW:
+                x = 1
+            elif j in traj_HW1:
+                x = 2
+            elif j in traj_HW2:
+                x = 3
+            res = np.round((j - traj_not_water[-1] - x)/3) + res_not_water + 1
+            if res not in water_res_neighbors_i: #remove duplicate residues
+                water_res_neighbors_i.append(res)
+        water_res_neighbors.append(water_res_neighbors_i)
+
+    #Determine number of water neighbors in each frame
+    num_neighbors = []
+    for i in range(len(water_res_neighbors)):
+        num_neighbors.append(len(water_res_neighbors[i]))
+
+    mean_neighbors = np.mean(num_neighbors)
+
+    #Get list of all unique neighboring water atoms
+    water_res = []
+    for i in range(len(water_res_neighbors)):#Cycle through each frame
+        indices = water_res_neighbors[i]#Seperate all contacts in that frame
+        for j in range(len(indices)):#Cycle through all contacts and seperate those with water molecules
+            if indices[j] not in water_res:
+                water_res.append(indices[j])
+
+    #Determine if the water molecule interacts with the ligand too
+    if lig_res_water_neighbor != False:
+        water_res_lig_prot = []
+        for i in range(len(water_res_neighbors)): #Loop through frames
+            prot_res_water = water_res_neighbors[i]
+            lig_res_water = lig_res_water_neighbor[i]
+            check = 0
+            for j in prot_res_water:
+                if j in lig_res_water:
+                    check = 1
+                    break
+            water_res_lig_prot.append(check)
+
+    if lig_res_water_neighbor == False:
+        return water_res, mean_neighbors, water_res_neighbors
+    else:
+        return water_res, mean_neighbors, water_res_lig_prot
 
 def prot_water_dist(traj, prot_res, offset, water_indices):
     from itertools import product
@@ -36,8 +95,12 @@ def prot_water_dist(traj, prot_res, offset, water_indices):
 
     #Determine all solvent protein residue pairs
     prot_solv = list(product(prot_ind, water_indices))
-
-    #Determine distance b/w all protein residues and all solvent molecules
-    dist = md.compute_distances(traj, prot_solv, periodic=True)
+    
+    #Don't compute distances if no water molecules selected
+    if not water_indices:
+        dist = 0
+    else:
+        #Determine distance b/w all protein residues and all solvent molecules
+        dist = md.compute_distances(traj, prot_solv, periodic=True)
 
     return dist
