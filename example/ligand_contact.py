984ad87483158128e3861f7227809c8e6db86852
Anika Friedman <anika.friedman@colorado.edu>
2022-11-14 10:30:43 -0500

    Change output files

--- example/ligand_contact.py
+++ example/ligand_contact.py
@@ -13,7 +13,8 @@ parser.add_argument('-t', required=True, help='File name for input trajectory')
 parser.add_argument('-g', required=True, help= 'File name for input topology (gro format)')
 parser.add_argument('-m', required=False, type=int, default = 0, help= 'Supply the number of missing terminal residues(default 0)')
 parser.add_argument('-l', required=True, type = int, help= 'Ligand residue number')
-parser.add_argument('-sect', required=True, help= 'File containing sections of interest(txt)')
+parser.add_argument('-ln', required=False, default = 'LIG', type = str, help= 'Ligand name in GRO file')
+parser.add_argument('-sect', required=False, default = 'none', help= 'File containing sections of interest(txt)')
 
 #Import Arguments
 args = parser.parse_args()
@@ -25,8 +26,9 @@ if File_gro.split('.')[-1] != 'gro': #Add default file extension if not in input
     File_gro = File_gro + '.gro'
 miss_res = args.m
 lig = args.l
+lig_name = args.ln
 sect = args.sect
-if sect.split('.')[-1] != 'txt': #Add default file extension if not in input
+if sect.split('.')[-1] != 'txt' and sect != 'none': #Add default file extension if not in input
     sect = sect + '.txt'
 
 #Source custom functions
@@ -42,7 +44,7 @@ top = traj_ns.topology
 del traj
 
 #Determine indices of protein and ligand residues in topology
-lig_res = load_data.lig_check(lig, miss_res, traj_ns)
+lig_res = load_data.lig_check(lig, miss_res, traj_ns, lig_name)
 prot_res = np.linspace(0, lig_res-1, num = lig_res)
 
 #Determine distance between all protein residues and ligand
@@ -67,23 +69,24 @@ for i in range(0, len(prot_res)):
     if per_contact[i] > 75:
         output_high_contact.write(str(prot_res[i]+1+miss_res) + '\n')
 
-#Compute the number of simultaneous contacts per section
-sections = open(sect, 'r').readlines()#Load sections of interest file
+if sect != 'none':
+    #Compute the number of simultaneous contacts per section
+    sections = open(sect, 'r').readlines()#Load sections of interest file
 
-#Loop through sections of interest
-for i in range(len(sections)):
-    name1, name2, sect1, sect2 = load_data.read_sections(sections, i, miss_res, 1)
-    res_pairs = list(product([lig_res], sect1))
+    #Loop through sections of interest
+    for i in range(len(sections)):
+        name1, name2, sect1, sect2 = load_data.read_sections(sections, i, miss_res, 1, 2)
+        res_pairs = list(product([lig_res], sect1))
 
-    [dist, pairs] = md.compute_contacts(traj_ns, contacts=res_pairs, scheme='closest-heavy', ignore_nonprotein = False, periodic=True, soft_min = False)
+        [dist, pairs] = md.compute_contacts(traj_ns, contacts=res_pairs, scheme='closest-heavy', ignore_nonprotein = False, periodic=True, soft_min = False)
     
-    #Determine simultaenrous contacts
-    frames, pairs = np.shape(dist)
-    simul_contact = np.zeros(frames)
-    for t in range(frames):
-        for r in range(pairs):
-            if dist[t][r] < 0.4:
-                simul_contact[t] += 1
-    np.savetxt('lig_inter/' + name1 + '_lig_contacts.txt', simul_contact)
+        #Determine simultaenrous contacts
+        frames, pairs = np.shape(dist)
+        simul_contact = np.zeros(frames)
+        for t in range(frames):
+            for r in range(pairs):
+                if dist[t][r] < 0.4:
+                    simul_contact[t] += 1
+        np.savetxt('lig_inter/' + name1 + '_lig_contacts.txt', simul_contact)
 
 print('Ligand Interaction Analysis Complete')
