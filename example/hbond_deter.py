1bf7e984658e8676f8a3b4c585a93685b44ee65f
Anika Friedman <anika.friedman@colorado.edu>
2022-11-18 10:55:36 -0500

    Remove correlated frames and fix errors

--- example/hbond_deter.py
+++ example/hbond_deter.py
@@ -7,10 +7,11 @@ import sys
 import os.path
 
 #Declare arguments
-parser = argparse.ArgumentParser(description = 'Determination of DSSP for MD Trajectory')
+parser = argparse.ArgumentParser(description = 'Determination all h-bonds present more than set percent of the trajectory')
 parser.add_argument('-t', required=True, help='File name for input trajectory')
 parser.add_argument('-g', required=True, help= 'File name for input topology (gro format)')
 parser.add_argument('-m', required=False, type=int, default = 0, help= 'Supply the number of missing terminal residues(default 0)')
+parser.add_argument('-f', required=False, type=int, default = 0.6, help= 'Minimum frequency h-bond appears(0 to 1)')
 
 #Import Arguments
 args = parser.parse_args()
@@ -21,23 +22,26 @@ File_gro = args.g
 if File_gro.split('.')[-1] != 'gro': #Add default file extension if not in input
     File_gro = File_gro + '.gro'
 miss_res = args.m
+freq_set = args.f
 
 #Source custom functions
 current_directory = os.path.dirname(os.path.realpath(__file__))
 prefix = current_directory.rsplit('/',1)[0]
 sys.path.insert(1, prefix + '/Traj_process/')
 import load_data 
-import process_traj
+import hbond
 
 #Load Trajectory
 traj = load_data.mdtraj_load(File_traj, File_gro)
-traj_ns = traj.remove_solvent()
+traj_uncorr = load_data.remove_uncorr('uncorrelated_frames.txt', traj)#Limit to uncorrelated frames
+traj_ns = traj_uncorr.remove_solvent() #Remove solvent from the trajectory leaving only protein (and ligand if applicable)
+del traj; del traj_uncorr #Save space
 
 #Set protein offset based on missing residues
 offset = 1 + miss_res
 
 #Determine list of H-bonds present in the trajectory for over 60% of the frames
-hbonds = md.baker_hubbard(traj_ns, freq=0.6, exclude_water=True, periodic=False)
+hbonds = md.baker_hubbard(traj_ns, freq=freq_set, exclude_water=True, periodic=False)
 label = lambda hbond : '%s -- %s' % (traj_ns.topology.atom(hbond[0]), traj_ns.topology.atom(hbond[2])) #Extract labels for h-bonds
 np.savetxt('Hbonds_atoms.txt', hbonds) #Save all atom indicies of h-bonds to file for further analysis
 
@@ -50,6 +54,6 @@ for hbond in hbonds:
 file_object.close() #close file
 
 #Determine the exact percentage of time that each h-bond present for >60% of the trajectory is formed
-per = process_traj.hbond_per(traj_ns, hbonds)
+per = hbond.bond_per(traj_ns, hbonds)
 np.savetxt('Hbonds_per.txt', per)
 
